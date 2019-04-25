d555d6a6db7aa010f3020714120d5825d1486488
thomas <thomasbshop@gmail.com>
2019-04-25 19:59:03 +0300

    hackathon api

---
+++ retina/uploadapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+# from .models import File
+
+class File(models.Model):
+    file = models.FileField(blank=False, null=False)
+    def __str__(self):
+        return self.file.name
