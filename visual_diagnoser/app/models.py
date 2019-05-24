8a034b1aae5969d424f8c7d038458ae4dae60d73
thomas <thomasbshop@gmail.com>
2019-05-24 11:17:07 +0300

    further changes

---
+++ visual_diagnoser/app/models.py
@@ -0,0 +1,48 @@
+# ourapp/models.py
+
+from . import db, ma
+from datetime import datetime
+# from config import db, ma
+
+
+class ImageProfile(db.Model):
+    """Details needed with the image to be stored in the db"""
+    __tablename__ = "profiles"
+    image_id = db.Column(db.Integer, primary_key=True)
+    lname = db.Column(db.String(32))
+    fname = db.Column(db.String(32))
+    image_name = db.Column(db.String(100))
+    results = db.Column(db.String(64))
+    description = db.Column(db.String(400))
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+    def __init__(self, lname, fname, image_name, results, description):
+        self.lname = lname
+        self.fname = fname
+        self.image_name = image_name
+        self.results = results
+        self.description = description
+
+    def __repr__(self):
+        return f"<image_id: {self.image_id}"
+
+
+class ImageProfileSchema(ma.ModelSchema):
+    class Meta:
+        model = ImageProfile
+        sqla_session = db.session
+        # fields = (
+        #     'image_id', 
+        #     'lname', 
+        #     'fname',
+        #     'image_name',
+        #     'results',
+        #     'description',
+        #     'timestamp')
+
+
+
+image_schema = ImageProfileSchema(strict=True)
+images_schema = ImageProfileSchema(many=True, strict=True)
---
+++ visual_diagnoser/app/views.py
@@ -0,0 +1,140 @@
+import os
+from flask import Flask, flash, jsonify, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+
+# custom
+from . import app
+from . import db
+from . import models
+from .ai_model.make_prediction import predict
+
+
+# port
+# port = int(os.getenv("PORT", 9099))
+# port = int(os.getenv("VCAP_APP_PORT"))
+
+# Use os.getenv("key") to get environment variables
+# app_name = os.getenv("APP_NAME")
+
+# Use os.getenv("key") to get environment variables
+app_name = os.getenv("APP_NAME")
+
+# session
+app.config['SESSION_TYPE'] = 'filesystem'
+
+# upload folder
+basedir = app.config["BASEDIR"]
+UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+# UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+
+
+# fun to check the file type
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                                filename)
+
+# post the image.
+@app.route('/api/upload', methods=['GET', 'POST'])
+def add_profile():
+	if request.method == 'POST':
+		# check if the post request has the file part
+	    if 'image' not in request.files:
+	        flash('No file part')
+	        # return redirect(request.url)
+	        return jsonify({"description":"no file part"})
+	        
+	    file = request.files['image']
+	    # if user does not select file, browser also
+	    # submit an empty part without filename
+	    if file.filename == '':
+	        flash('No selected file')
+	        # return redirect(request.url)
+	        return jsonify({"description":"no selected file"})
+
+	    if file and allowed_file(file.filename):
+	        filename = secure_filename(file.filename)
+	        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	        # data
+	        lname = request.form['lastname']
+	        fname = request.form['firstname']
+	        results = request.form['results']
+	        description = request.form['description']
+	        image_name = filename
+
+	        new_profile = models.ImageProfile(lname, fname, image_name, results, description)
+
+	        db.session.add(new_profile)
+	        db.session.commit()
+
+	        response = new_profile
+
+	        return models.image_schema.jsonify(response)
+	        # return redirect(url_for('uploaded_file', filename=filename))
+	    else:
+	    	print("That file extension is not allowed")
+	    	return redirect(request.url)
+
+	return render_template("upload.html")
+
+
+# predict.
+@app.route('/api/predict', methods=['GET', 'POST'])
+def new_prediction():
+	if request.method == 'POST':
+		# check if the post request has the file part
+	    if 'image' not in request.files:
+	        flash('No file part')
+	        # return redirect(request.url)
+	        return jsonify({"description":"no file part"})
+	        
+	    file = request.files['image']
+	    # if user does not select file, browser also
+	    # submit an empty part without filename
+	    if file.filename == '':
+	        flash('No selected file')
+	        # return redirect(request.url)
+	        return jsonify({"description":"no selected file"})
+
+	    if file and allowed_file(file.filename):
+	        filename = secure_filename(file.filename)
+	        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	        # data
+	        lname = request.form['lastname']
+	        fname = request.form['firstname']
+	        description = request.form['description']
+	        image_name = filename
+	        # get the file path
+	        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+	        # make the prediction
+	        prediction = predict(file_path)
+
+
+	        new_profile = models.ImageProfile(lname, fname, image_name, str(prediction), description)
+
+	        db.session.add(new_profile)
+	        db.session.commit()
+
+	        response = new_profile
+
+	        return models.image_schema.jsonify(response)
+	        # return redirect(url_for('uploaded_file', filename=filename))
+	    else:
+	    	print("That file extension is not allowed")
+	    	return redirect(request.url)
+
+	return render_template("upload.html")
